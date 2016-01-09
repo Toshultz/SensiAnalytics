d6279f1a1a050e7edc69bc7cee5228c8a5688737
Toshultz <toshultz@gmail.com>
2016-01-08 17:18:14 -0800

    update init

---
+++ init.r
@@ -0,0 +1,9 @@
+install.packages("shiny")
+install.packages("data.table")
+install.packages("tidyr")
+install.packages("ggvis")
+
+library(shiny)
+library(data.table)
+library(tidyr)
+require(ggvis)
